1b44b238fd04412bdc6de87659cebf1d76af5f7a
Ankur Singh <ankursingh9806@gmail.com>
2024-04-27 09:38:09 +0530

    create crudcrud.js

---
+++ crudcrud.js
@@ -0,0 +1,106 @@
+const form = document.getElementById("my-form");
+const userList = document.getElementById("user-list");
+
+form.addEventListener("submit", addAppointmentData);
+
+async function loadAppointmentData() {
+    try {
+        const res = await axios.get("https://crudcrud.com/api/3d233eeac5d34d168f56f6d9f1872466/users");
+        res.data.forEach((userData) => {
+            showOnScreen(userData);
+        })
+    } catch (err) {
+        console.error("failed to load users:", err);
+    }
+}
+loadAppointmentData();
+
+async function addAppointmentData(e) {
+    try {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const phone = e.target.phone.value;
+        const userData = {
+            name: name,
+            email: email,
+            phone: phone
+        };
+        const res = await axios.post("https://crudcrud.com/api/3d233eeac5d34d168f56f6d9f1872466/users", userData);
+        showOnScreen(userData);
+        form.reset();
+    } catch (err) {
+        console.error("failed to save user:", err);
+    }
+}
+
+function showOnScreen(user) {
+    const listItem = document.createElement("li");
+    const textNode = document.createTextNode(`${user.name} ${user.email} ${user.phone}`);
+    listItem.appendChild(textNode);
+
+    const deleteButton = document.createElement("button");
+    const editButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    editButton.innerHTML = "Edit";
+
+    let userId = user._id;
+    deleteButton.addEventListener("click", function () {
+        deleteAppointmentData(user, userId, listItem);
+    });
+    editButton.addEventListener("click", function () {
+        updateAppointmentData(user, userId, listItem);
+    });
+
+    listItem.appendChild(deleteButton);
+    listItem.appendChild(editButton);
+    userList.appendChild(listItem);
+}
+
+async function deleteAppointmentData(user, userId, listItem) {
+    try {
+        const res = await axios.delete(`https://crudcrud.com/api/3d233eeac5d34d168f56f6d9f1872466/users/${userId}`);
+        userList.removeChild(listItem);
+    } catch (err) {
+        console.error("failed to delete user:", err);
+    }
+}
+
+async function updateAppointmentData(user, userId, listItem) {
+    try {
+        document.getElementById("name").value = user.name;
+        document.getElementById("email").value = user.email;
+        document.getElementById("phone").value = user.phone;
+
+        form.removeEventListener("submit", addAppointmentData);
+        form.addEventListener("submit", editAppointmentData);
+
+        async function editAppointmentData(e) {
+            try {
+                e.preventDefault();
+                const updatedName = e.target.name.value;
+                const updatedEmail = e.target.email.value;
+                const updatedPhone = e.target.phone.value;
+
+                const updatedUserData = {
+                    name: updatedName,
+                    email: updatedEmail,
+                    phone: updatedPhone
+                };
+
+                const res = await axios.put(`https://crudcrud.com/api/3d233eeac5d34d168f56f6d9f1872466/users/${userId}`, updatedUserData);
+                user.name = updatedName;
+                user.email = updatedEmail;
+                user.phone = updatedPhone;
+
+                listItem.firstChild.textContent = `${updatedName} ${updatedEmail} ${updatedPhone}`;
+
+                form.reset();
+            } catch (err) {
+                console.error("failed to edit user:", err);
+            }
+        }
+    } catch (err) {
+        console.error("failed to update user:", err);
+    }
+}
